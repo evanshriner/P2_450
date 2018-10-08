d0491a4329c1c094c1394ac20036bf9c0a590f39
Evan Shriner <eshriner@umich.edu>
2018-10-07 21:30:26 -0400

    extra stuff

---
+++ MLFQ.h
@@ -0,0 +1,30 @@
+
+// the number of queues in the MLFQ
+#define NQUEUE = 6;
+
+// the constant time quantum increase for each queue
+// since timer intterupt happens every 10ms, divide
+// this value by 10.
+#define TQ = 1;
+
+
+struct MLFQ {
+
+    // pointer to queue array
+    struct queue *queues[NQUEUE];
+
+};
+
+struct queue {
+
+    // array of processes
+    struct proc *q[NPROC];
+
+    // amount of processes
+    int size;
+
+    // time quantum of selected queue
+    int quantum; // this is assigned during initalization of proc.c / scheduler
+
+
+};
